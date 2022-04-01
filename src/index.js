652625ce0970c90f96d63d4e96897ece1315eb9c
JuniorCoder <thomas08samuel@gmail.com>
2022-04-01 21:14:50 +0200

    Component setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './globals.sass';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+root.render(<App />);
