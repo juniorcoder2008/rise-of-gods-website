48ca98b94f27aed29c7a99f8fc2b837bcca0318d
JuniorCoder <thomas08samuel@gmail.com>
2022-04-02 12:12:08 +0200

    Discord server

--- src/components/Discord/Discord.jsx
+++ src/components/Discord/Discord.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import DiscordIcon from '../../assets/DiscordIcon';
+import './Discord.sass';
 
 const Discord = () => {
   return (
-    <div id='discord'>Discord</div>
+    <div id='discord'>
+      <h1>Discord Server</h1>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo</p>
+
+      <button><DiscordIcon /> Join Server</button>
+    </div>
   )
 }
 
